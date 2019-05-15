27674b6fe34bb11f91be279d4f45888e7c9f2fd3
Teemo Tebest <teemo.tebest@gmail.com>
2019-05-15 11:34:37 +0200

    Implemented further

--- src/jsx/Vis.jsx
+++ src/jsx/Vis.jsx
@@ -22,37 +22,93 @@ class Vis extends Component {
     super();
 
     this.state = {
+      selected_country:'',
       year_month_idx:0
     }
   }
   componentDidMount() {
+    let self = this;
+
     let width = 320;
     let height = 500;
 
     let projection = d3.geoMercator().center([58,57]).scale(400);
     
-    let svg = d3.select('.map').append('svg').attr('width', width).attr('height', height);
+    let svg = d3.select('.' + style.map).append('svg').attr('width', width).attr('height', height);
     path = d3.geoPath().projection(projection);
     g = svg.append('g');
 
-    let self = this;
+    let offsetL = document.getElementsByClassName(style.map)[0].offsetLeft + 10;
+    let offsetT = document.getElementsByClassName(style.map)[0].offsetTop + 10;
+    let tooltip = d3.select('.' + style.map)
+      .append('div')
+      .attr('class', style.hidden + ' ' + style.tooltip);
+    function showTooltip (d) {
+      let country = d.properties.NAME;
+      let max = {
+        date:'',
+        value:0
+      };
+      _.each(self.props.data[country], (value, date) => {
+        if (value > max.value) {
+          max.value = value,
+          max.date = date;
+        }
+      });
+      let mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+      if (self.props.data[country]) {
+        tooltip.classed(style.hidden, false)
+          .attr('style', 'left: ' + (mouse[0] + offsetL) +  'px; top:' + (mouse[1] + offsetT) + 'px;')
+          .html('<h4>' + country + '</h4><p>Situation was the worst in ' + max.date.replace('M', '/') + ' with ' + max.value.toLocaleString() + ' asylum seekers.');
+      }
+    }
+    function selected (d) {
+      if (self.props.data[d.properties.NAME]) {
+        if (!d3.select(this).classed(style.selected)) {
+          self.setState((state, props) => ({
+            selected_country: d.properties.NAME
+          }));
+          d3.select('.' + style.selected_country).classed(style.hidden, false);
+          d3.select('.' + style.selected).classed(style.selected, false);
+          d3.select(this.parentNode.appendChild(this)).classed(style.selected, true);
+        }
+        else {
+          d3.select('.' + style.selected_country).classed(style.hidden, true);
+          d3.select('.' + style.selected).classed(style.selected, false);
+          self.setState((state, props) => ({
+            selected_country: ''
+          }));
+        }
+      }
+      else {
+        self.setState((state, props) => ({
+          selected_country: ''
+        }));
+      }
+    }
+
     d3.json('./data/europe.topojson').then(function(topology) {
       g.selectAll('path').data(topojson.feature(topology, topology.objects.europe).features)
         .enter()
         .append('path')
         .attr('d', path)
+        .on('mousemove', showTooltip)
+        .on('mouseout', function (d,i) {
+          tooltip.classed(style.hidden, true);
+         })
+        .on('click', selected)
         .attr('class', style.path)
         .attr('fill', function(d, i) {
           return self.getCountryColor(d.properties.NAME);
         });
-      self.text = g.append('text')
+      self.text = svg.append('text')
         .attr('alignment-baseline', 'middle')
         .attr('dy', '.35em')
         .attr('class', style.text)
         .attr('text-anchor', 'middle')
-        .attr('x', '45%')
+        .attr('x', '50%')
         .attr('y', '55%')
-        .text(self.dates[self.state.year_month_idx].replace('M', '/'));
+        .html(self.dates[self.state.year_month_idx].replace('M', '/'));
     });
     setTimeout(() => {
       this.createInterval();
@@ -78,9 +134,8 @@ class Vis extends Component {
   }
   getCountryColor(country) {
     if (this.props.data[country] !== undefined) {
-      let max = _.max((_.values(this.props.data[country])));
       if (this.props.data[country][this.dates[this.state.year_month_idx]] > 100) {
-        return 'rgba(255, 0, 0, ' + ((this.props.data[country][this.dates[this.state.year_month_idx]]) / 1) / max + ')';
+        return 'rgba(255, 0, 0, ' + ((this.props.data[country][this.dates[this.state.year_month_idx]]) / 1) / _.max((_.values(this.props.data[country]))) + ')';
       }
       else {
         return '#fff';
@@ -93,7 +148,6 @@ class Vis extends Component {
   changeCountryColor(type) {
     let self = this;
     g.selectAll('path').attr('d', path)
-      .attr('class', style.path_notransition)
       .attr('fill', function(d, i) {
         return self.getCountryColor(d.properties.NAME);
       });
@@ -113,7 +167,17 @@ class Vis extends Component {
   render() {
     this.dates = _.keys(this.props.data['Finland']);
     if (this.text) {
-      this.text.text(this.dates[this.state.year_month_idx].replace('M', '/'));
+      this.text.html(this.dates[this.state.year_month_idx].replace('M', '/'));
+    }
+    let selected_value = 0;
+    if (this.state.selected_country && this.props.data[this.state.selected_country]) {
+      selected_value = this.props.data[this.state.selected_country][this.dates[this.state.year_month_idx]];
+      if (selected_value === -1) {
+        this.text.html(this.dates[this.state.year_month_idx].replace('M', '/') + ' ' + this.state.selected_country + ' –');
+      }
+      else {
+        this.text.html(this.dates[this.state.year_month_idx].replace('M', '/') + ' ' + this.state.selected_country + ' ' + selected_value.toLocaleString());
+      }
     }
     return (
       <div>
@@ -121,12 +185,12 @@ class Vis extends Component {
           className={style.slider_container}
           dots={false}
           max={this.dates.length - 1}
+          onAfterChange={this.onAfterSliderChange.bind(this)}
           onBeforeChange={this.onBeforeSliderChange}
           onChange={this.onSliderChange.bind(this)}
-          onAfterChange={this.onAfterSliderChange.bind(this)}
           value={this.state.year_month_idx}
         />
-        <div className="map"></div>
+        <div className={style.map}></div>
       </div>
     );
   }
