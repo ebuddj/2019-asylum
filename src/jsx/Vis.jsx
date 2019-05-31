af4d54b3664330468cd44a93c7921ee50df4e6da
Teemo Tebest <teemo.tebest@gmail.com>
2019-05-31 10:55:47 +0200

    Updated geo parameters

--- src/jsx/Vis.jsx
+++ src/jsx/Vis.jsx
@@ -37,9 +37,9 @@ class Vis extends Component {
     let self = this;
 
     let width = 320;
-    let height = 500;
+    let height = 420;
 
-    let projection = d3.geoAzimuthalEquidistant().center([58,57]).scale(400);
+    let projection = d3.geoAzimuthalEquidistant().center([52,44]).scale(550);
     
     let svg = d3.select('.' + style.map_container).append('svg').attr('width', width).attr('height', height);
     path = d3.geoPath().projection(projection);
